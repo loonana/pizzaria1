4015f642fe4d258fa988231bc685013ecd1e6904
loonana <itsloye@gmail.com>
2024-08-06 11:49:10 -0300

    pizzas

---
+++ src/shared/Pizzas.jsx
@@ -0,0 +1,40 @@
+import mussarela from "../assets/images/pizza-mussarela.jpeg"
+import calabresa from "../assets/images/pizza-calabresa2.jpeg"
+import atum from "../assets/images/pizza-atum.jpeg"
+import portuguesa from "../assets/images/pizza-portuguesa.jpeg"
+import frango from "../assets/images/pizza-frango.jpeg"
+
+const Pizzas = [
+    {
+      "id": 1,
+      "nome": "Mussarela",
+      "descricao": "Molho de tomate e mussarela.",
+      "imagem": mussarela
+    },
+    {
+      "id": 2,
+      "nome": "Frango com Catupiry",
+      "descricao": "Molho de tomate, mussarela, frango desfiado e catupiry.",
+      "imagem": frango
+    },
+    {
+      "id": 3,
+      "nome": "Portuguesa",
+      "descricao": "Molho de tomate, mussarela, presunto, ovo, cebola, azeitona e orégano.",
+      "imagem": portuguesa
+    },
+    {
+      "id": 4,
+      "nome": "Calabresa",
+      "descricao": "Molho de tomate, mussarela, calabresa e cebola.",
+      "imagem": calabresa
+    },
+    {
+      "id": 5,
+      "nome": "Atum",
+      "descricao": "Molho de tomate, mussarela, atum e tomate.",
+      "imagem": atum
+    },
+  ]
+
+  export default Pizzas;
